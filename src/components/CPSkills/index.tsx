7d0c4f6770855d0a7014e7caf2b804815fa9ac90
wilson castro <43712100+whccc@users.noreply.github.com>
2023-03-23 11:44:27 -0500

    hoja de vida

--- src/components/CPSkills/index.tsx
+++ src/components/CPSkills/index.tsx
@@ -1,73 +1,82 @@
 import {
-    DivContainerSkills,
-    DivContainerDataInfo,
-    DivDataInfo,
-    H1DataInfo,
-    PDataInfo,
-    DivContainerSkill,
-    DivContainerSkillFrontend,
-    DivContainerFrontend,
-    ImgSkill,
-    H2Title
-} from './styles'
+  DivContainerSkills,
+  DivContainerDataInfo,
+  DivDataInfo,
+  H1DataInfo,
+  PDataInfo,
+  DivContainerSkill,
+  DivContainerSkillFrontend,
+  DivContainerFrontend,
+  ImgSkill,
+  H2Title,
+  ButtonHv,
+  WrapperHv,
+} from "./styles";
 const CPSkils = () => {
-    return (
-        <DivContainerSkills>
-            <H1DataInfo>Información</H1DataInfo>
-            <DivContainerDataInfo>
-                <DivDataInfo>
-                    <PDataInfo>Information Systems Technologist</PDataInfo>
-                    <PDataInfo>Medellín-Colombia</PDataInfo>
-                    <PDataInfo>wilsoncastro0710@gmail.com</PDataInfo>
-                </DivDataInfo>
-                <p>
-                    Soy una persona responsable, creativa, con iniciativa y
-                    puntualidad, asumo con agrado los retos y metas; con buen
-                    manejo de relaciones interpersonales, facilidad para
-                    trabajar en equipo, en condiciones de alta presión, así como
-                    para resolver problemas eficientemente. Amante a la
-                    programación y muy autodidacta.
-                </p>
-            </DivContainerDataInfo>
-            <br />
-            <div>
-                <H1DataInfo>Habilidades</H1DataInfo>
-                <DivContainerSkill>
-                    <DivContainerFrontend>
-                        <H2Title>Frontend</H2Title>
-                        <DivContainerSkillFrontend>
-                            <ImgSkill src="/images/ImgFrontendHtml.jpg" />
-                            <ImgSkill src="/images/ImgFrontendJs.jpg" />
-                            <ImgSkill src="/images/ImgFrontendCss.jpg" />
-                            <ImgSkill src="/images/ImgFrontendReact.jpg" />
-                            <ImgSkill src="/images/ImgFrontendNextJs.jpg" />
-                            <ImgSkill src="/images/ImgFrontendAngular.jpg" />
-                        </DivContainerSkillFrontend>
-                    </DivContainerFrontend>
+  return (
+    <DivContainerSkills>
+      <H1DataInfo>Información</H1DataInfo>
+      <DivContainerDataInfo>
+        <DivDataInfo>
+          <PDataInfo>Information Systems Technologist</PDataInfo>
+          <PDataInfo>Medellín-Colombia</PDataInfo>
+          <PDataInfo>wilsoncastro0710@gmail.com</PDataInfo>
+        </DivDataInfo>
+        <p>
+          Soy una persona responsable, creativa, con iniciativa y puntualidad,
+          asumo con agrado los retos y metas; con buen manejo de relaciones
+          interpersonales, facilidad para trabajar en equipo, en condiciones de
+          alta presión, así como para resolver problemas eficientemente. Amante
+          a la programación y muy autodidacta.
+        </p>
+      </DivContainerDataInfo>
+      <br />
 
-                    <DivContainerFrontend>
-                        <H2Title>Backend</H2Title>
-                        <DivContainerSkillFrontend>
-                            <ImgSkill src="/images/ImgBackendNode.jpg" />
-                            <ImgSkill src="/images/ImgBackendExpress.jpg" />
-                            <ImgSkill src="/images/ImgBackendAdonis.jpg" />
-                            <ImgSkill src="/images/Nestjs.png" />
-                        </DivContainerSkillFrontend>
-                    </DivContainerFrontend>
+      <WrapperHv>
+        <H1DataInfo>Hoja de vida</H1DataInfo>
+        <ButtonHv target="_blank" href="/HV-Wilson herney castro cabrera.pdf">
+          Descargar...
+        </ButtonHv>
+      </WrapperHv>
 
-                    <DivContainerFrontend>
-                        <H2Title>Database</H2Title>
-                        <DivContainerSkillFrontend>
-                            <ImgSkill src="/images/ImgSqlMysql.jpg" />
-                            <ImgSkill src="/images/ImgSqlSqlServer.jpg" />
-                            <ImgSkill src="/images/ImgSqlMongoDb.jpg" />
-                            <ImgSkill src="/images/PostgreSQL.png" />
-                        </DivContainerSkillFrontend>
-                    </DivContainerFrontend>
-                </DivContainerSkill>
-            </div>
-        </DivContainerSkills>
-    )
-}
+      <div>
+        <H1DataInfo>Habilidades</H1DataInfo>
+        <DivContainerSkill>
+          <DivContainerFrontend>
+            <H2Title>Frontend</H2Title>
+            <DivContainerSkillFrontend>
+              <ImgSkill src="/images/ImgFrontendHtml.jpg" />
+              <ImgSkill src="/images/ImgFrontendJs.jpg" />
+              <ImgSkill src="/images/ImgFrontendCss.jpg" />
+              <ImgSkill src="/images/ImgFrontendReact.jpg" />
+              <ImgSkill src="/images/ImgFrontendNextJs.jpg" />
+              <ImgSkill src="/images/ImgFrontendAngular.jpg" />
+            </DivContainerSkillFrontend>
+          </DivContainerFrontend>
 
-export default CPSkils
+          <DivContainerFrontend>
+            <H2Title>Backend</H2Title>
+            <DivContainerSkillFrontend>
+              <ImgSkill src="/images/ImgBackendNode.jpg" />
+              <ImgSkill src="/images/ImgBackendExpress.jpg" />
+              <ImgSkill src="/images/ImgBackendAdonis.jpg" />
+              <ImgSkill src="/images/Nestjs.png" />
+            </DivContainerSkillFrontend>
+          </DivContainerFrontend>
+
+          <DivContainerFrontend>
+            <H2Title>Database</H2Title>
+            <DivContainerSkillFrontend>
+              <ImgSkill src="/images/ImgSqlMysql.jpg" />
+              <ImgSkill src="/images/ImgSqlSqlServer.jpg" />
+              <ImgSkill src="/images/ImgSqlMongoDb.jpg" />
+              <ImgSkill src="/images/PostgreSQL.png" />
+            </DivContainerSkillFrontend>
+          </DivContainerFrontend>
+        </DivContainerSkill>
+      </div>
+    </DivContainerSkills>
+  );
+};
+
+export default CPSkils;
--- src/components/CPSkills/styles.ts
+++ src/components/CPSkills/styles.ts
@@ -1,90 +1,111 @@
-import styled from 'styled-components'
+import styled from "styled-components";
 
 export const DivContainerSkills = styled.div`
-    margin-top: 10px;
-`
+  margin-top: 10px;
+`;
 
 export const DivContainerDataInfo = styled.div`
-    padding: 10px;
-    text-align: center;
-    color: #fff;
-    border-radius: 10px;
-`
+  padding: 10px;
+  text-align: center;
+  color: #fff;
+  border-radius: 10px;
+`;
 
 export const DivDataInfo = styled.div`
-    text-align: center !important;
-    @media (min-width: 1024px) {
-        display: flex;
-        justify-content: center;
-        margin: 5px;
-        border-bottom: 1px solid #ddd;
+  text-align: center !important;
+  @media (min-width: 1024px) {
+    display: flex;
+    justify-content: center;
+    margin: 5px;
+    border-bottom: 1px solid #ddd;
 
-        & p:nth-child(2) {
-            &::before {
-                content: '|';
-                margin-right: 10px;
-                height: 20px;
-                overflow: hidden;
-                display: inline-block;
-                vertical-align: middle;
-            }
-            &::after {
-                content: '|';
-                margin-left: 10px;
-                height: 20px;
-                overflow: hidden;
-                display: inline-block;
-                vertical-align: middle;
-            }
-        }
+    & p:nth-child(2) {
+      &::before {
+        content: "|";
+        margin-right: 10px;
+        height: 20px;
+        overflow: hidden;
+        display: inline-block;
+        vertical-align: middle;
+      }
+      &::after {
+        content: "|";
+        margin-left: 10px;
+        height: 20px;
+        overflow: hidden;
+        display: inline-block;
+        vertical-align: middle;
+      }
     }
-`
+  }
+`;
 
 export const H1DataInfo = styled.h2`
-    text-align: center;
-    color: #fff;
-    font-weight: 900;
-`
+  text-align: center;
+  color: #fff;
+  font-weight: 900;
+`;
 
 export const PDataInfo = styled.p`
-    margin: 0px;
-    line-height: 18px;
-    @media (min-width: 1024px) {
-        margin-right: 10px;
-        font-size: 20px;
-    }
-`
+  margin: 0px;
+  line-height: 18px;
+  @media (min-width: 1024px) {
+    margin-right: 10px;
+    font-size: 20px;
+  }
+`;
 
 export const DivContainerSkill = styled.div`
-    display: grid;
-    grid-template-columns: repeat(3, 1fr);
-    grid-template-rows: repeat(1, 1fr);
-    grid-gap: 10px;
-`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-template-rows: repeat(1, 1fr);
+  grid-gap: 10px;
+`;
 
 export const DivContainerSkillFrontend = styled.div`
-    display: grid;
-    grid-template-columns: repeat(3, 1fr);
-    grid-template-rows: repeat(2, 1fr);
-    grid-gap: 10px;
-    padding: 10px;
-`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-template-rows: repeat(2, 1fr);
+  grid-gap: 10px;
+  padding: 10px;
+`;
 export const DivContainerFrontend = styled.div`
-    border: 1px solid #ddd;
-    border-radius: 10px;
-    overflow: hidden;
+  border: 1px solid #ddd;
+  border-radius: 10px;
+  overflow: hidden;
 
-    background-color: #000;
-`
+  background-color: #000;
+`;
 export const H2Title = styled.h3`
-    background-color: #000;
-    color: #fff;
-    padding: 10px;
-    border-bottom: 1px solid #000;
-    text-align: center;
-`
+  background-color: #000;
+  color: #fff;
+  padding: 10px;
+  border-bottom: 1px solid #000;
+  text-align: center;
+`;
 export const ImgSkill = styled.img`
-    width: 100%;
-    height: 100%;
-    background-color: #fff;
-`
+  width: 100%;
+  height: 100%;
+  background-color: #fff;
+`;
+export const WrapperHv = styled.div`
+  border-bottom: 1px solid #fff;
+  border-top: 1px solid #fff;
+  margin-bottom: 20px;
+  text-align: center;
+  padding-bottom: 10px;
+  background-color: #fff;
+  & h2 {
+    color: #000 !important;
+  }
+`;
+export const ButtonHv = styled.a`
+  display: block;
+  margin: auto;
+  cursor: pointer;
+  width: 200px;
+  background-color: transparent;
+  color: #000;
+  border: 1px solid #000;
+  padding: 20px;
+`;
